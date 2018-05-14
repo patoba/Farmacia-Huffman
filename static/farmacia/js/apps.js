d2b89ce17d80bdcc0509f92ae00ed28d436baecd
ivyflammenwald <fek1814@gmail.com>
2018-05-13 19:18:52 -0500

    front

--- static/farmacia/js/apps.js
+++ static/farmacia/js/apps.js
@@ -4,6 +4,16 @@ $(document).ready(function(){
     $('.carousel').carousel();
     $('.materialboxed').materialbox();
     $('.slider').slider();
+ 
+
+
+ $('.slider').slider({
+      interval: 2000,
+  });
+
+
+
+
   });
 
 Materialize.updateTextFields();
